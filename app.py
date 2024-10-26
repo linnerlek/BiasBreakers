2b6c18679c2e615aa185f77addaea4827a86465e
Linn Erle <linnerlek@gmail.com>
2024-10-26 00:30:06 -0400

    updates

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return "Welcome to EmpowHer!"
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/howto')
+def howto():
+    return render_template('howto.html')
+
+
+@app.route('/scenario')
+def scenario():
+    return render_template('scenario.html')
 
 
 if __name__ == '__main__':
